78af3ebf4357122705bd2305b4a9e0cd56d86b7e
Yolis <elisstormbreaker@gmail.com>
2020-04-28 20:05:13 +0300

    Add gradle common dependencies files

---
+++ buildSrc/src/main/kotlin/GradleDependency.kt
@@ -0,0 +1,16 @@
+object GradlePluginVersion {
+    const val ANDROID_GRADLE = "3.5.3"
+    const val GRADLE_VERSION_PLUGIN = "0.22.0"
+    const val KOTLIN = CoreVersion.KOTLIN
+    const val SAFE_ARGS = CoreVersion.NAVIGATION
+}
+
+object GradlePluginId {
+    const val ANDROID_APPLICATION = "com.android.application"
+    const val ANDROID_DYNAMIC_FEATURE = "com.android.dynamic-feature"
+    const val ANDROID_LIBRARY = "com.android.library"
+    const val KOTLIN_JVM = "org.jetbrains.kotlin.jvm"
+    const val KOTLIN_ANDROID = "org.jetbrains.kotlin.android"
+    const val KOTLIN_ANDROID_EXTENSIONS = "org.jetbrains.kotlin.android.extensions"
+    const val SAFE_ARGS = "androidx.navigation.safeargs.kotlin"
+}
