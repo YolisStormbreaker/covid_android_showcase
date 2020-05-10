0cbc66f67a0aaedf1265164046a74c6d27436143
Yolis <elisstormbreaker@gmail.com>
2020-05-10 17:15:15 +0300

    Refactoring

--- buildSrc/src/main/kotlin/ModuleDependency.kt
+++ buildSrc/src/main/kotlin/ModuleDependency.kt
@@ -5,26 +5,27 @@ private const val FEATURE_PREFIX = ":features"
 // "Module" means "project" in terminology of Gradle API. To be specific each "Android module" is a Gradle "subproject"
 @Suppress("unused")
 object ModuleDependency {
-    // All consts are accessed via reflection
-    const val APP = ":app"
-/*    const val FEATURE_ALBUM = ":feature_album"
-    const val FEATURE_PROFILE = ":feature_profile"
-    const val FEATURE_FAVOURITE = ":feature_favourite"*/
-    const val LibraryCommon = ":libraries:common"
-    const val LibraryFirebaseAnalytics = ":libraries:firebase_analytics"
-    const val LibraryFirebaseAuth = ":libraries:firebase_auth"
-    const val LibraryGoogleMapsAndPlaces = ":libraries:google_maps_places"
-    const val DataSourceNetworkCovidStats = ":data_sources:network:covid_stats"
-    //const val LIBRARY_TEST_UTILS = ":library_test_utils"
+	// All consts are accessed via reflection
+	const val APP = ":app"
 
-    // False positive" function can be private"
-    // See: https://youtrack.jetbrains.com/issue/KT-33610
-    fun getAllModules() = ModuleDependency::class.memberProperties
-        .filter { it.isConst }
-        .map { it.getter.call().toString() }
-        .toSet()
+	/*    const val FEATURE_ALBUM = ":feature_album"
+		const val FEATURE_PROFILE = ":feature_profile"
+		const val FEATURE_FAVOURITE = ":feature_favourite"*/
+	const val LibraryCommon = ":libraries:common"
+	const val LibraryFirebaseAnalytics = ":libraries:firebase_analytics"
+	const val LibraryFirebaseAuth = ":libraries:firebase_auth"
+	const val LibraryGoogleMapsAndPlaces = ":libraries:google_maps_places"
+	const val DataSourceNetworkCovidStats = ":data_sources:network:covid_stats"
+	//const val LIBRARY_TEST_UTILS = ":library_test_utils"
 
-    fun getDynamicFeatureModules() = getAllModules()
-        .filter { it.startsWith(FEATURE_PREFIX) }
-        .toSet()
+	// False positive" function can be private"
+	// See: https://youtrack.jetbrains.com/issue/KT-33610
+	fun getAllModules() = ModuleDependency::class.memberProperties
+		.filter { it.isConst }
+		.map { it.getter.call().toString() }
+		.toSet()
+
+	fun getDynamicFeatureModules() = getAllModules()
+		.filter { it.startsWith(FEATURE_PREFIX) }
+		.toSet()
 }
