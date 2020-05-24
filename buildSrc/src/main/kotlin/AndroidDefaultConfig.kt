e6c866b78bd7d128cca3c32fc58aed9e020db058
Yolis <elisstormbreaker@gmail.com>
2020-05-25 00:22:20 +0300

    Version update

--- buildSrc/src/main/kotlin/AndroidDefaultConfig.kt
+++ buildSrc/src/main/kotlin/AndroidDefaultConfig.kt
@@ -5,7 +5,7 @@ object AndroidDefaultConfig {
 	const val BUILD_TOOLS_VERSION = "29.0.3"
 
 	const val VERSION_CODE = 1
-	const val VERSION_NAME = "0.0.1"
+	const val VERSION_NAME = "1.0.0"
 
 	const val ID = "com.yolisstorm.covidpulse"
 	const val TEST_INSTRUMENTATION_RUNNER = "android.support.test.runner.AndroidJUnitRunner"
